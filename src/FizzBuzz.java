563e918c278768169926e4e490975504012d34f4
k.pang119 <k.pang119@gmail.com>
2017-06-12 22:53:47 -0500

    Changed methods to be boolean

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,25 +3,24 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		for(int x = 1;x <= 100; x++){
-			if(x%3==0 && x%5==0){
-				fizzBuzz(x);
-			}else if(x%3==0){
-				fizz(x);
-			}else if(x%5==0){
-				buzz(x);
+			if(fizzBuzz(x)){
+				System.out.println(x+" FizzBuzz");
+			}else if(fizz(x)){
+				System.out.println(x+" Fizz");
+			}else if(buzz(x)){
+				System.out.println(x+" Buzz");
 			}else{
 				System.out.println(x);
 			}
 		}
 	}
-	public static void fizzBuzz(int x){
-		System.out.println(x+" FizzBuzz");
+	public static boolean fizzBuzz(int x){
+		return(x%3==0 && x%5==0);
 	}
-	public static void fizz(int x){
-		System.out.println(x+" Fizz");
+	public static boolean fizz(int x){
+		return(x%3==0);
 	}
-	public static void buzz(int x){
-		System.out.println(x+" Buzz");
-
+	public static boolean buzz(int x){
+		return(x%5==0);
 	}
 }
